9ecc7fbbe070cbc367aafdb3670e05e4c5816a39
GK71 <gautamk1994@gmail.com>
2022-11-03 14:38:40 +0530

    first message

---
+++ reverse.py
@@ -0,0 +1,32 @@
+# import urllib library
+from urllib.request import urlopen
+
+# import json
+import json
+
+# store the URL in url as
+# parameter for urlopen
+url = "http://127.0.0.1:5000"
+
+# store the response of URL
+response = urlopen(url)
+
+# storing the JSON response
+# from url in data
+data_json = json.loads(response.read())
+
+# print the json response
+#print(data_json)
+
+
+#jsn_object = json.loads(data_json)
+
+# printing keys and values
+for i in data_json:
+   if i == 'message':
+      value = data_json[i]
+
+
+#print(value)
+print(value[::-1])
+
